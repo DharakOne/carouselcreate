7effe32afa79a5bf71e9aa35fd186a0aea90de02
DharakOne <juniorbenjamin2024@gmail.com>
2020-09-24 23:27:20 +0000

    Finalizado

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Styled from "styled-components"
+import Testimonials from "./Testimonials"
+
+
+export default function App(){
+    return(
+        <div>
+            <Testimonials/>
+        </div>
+    )
+}
